6a490aafa22fdba698b461096cf7c9c200eaa38d
Kinu <Davidotherworld@gmail.com>
2026-02-18 13:26:32 +0100

    Añadido Default options: classes (CSS), tag (Base), position (Tamaño) y actions (para botones)

--- module/sheets/character.mjs
+++ module/sheets/character.mjs
@@ -1,5 +1,15 @@
-export default class characterSheet extends ActorSheet {
-  getData() {
-    
+const {api, sheets} = foundry.applications;
+
+export default class characterSheet extends api.HandlebarsApplicationMixin(sheets.ActorSheetV2) {
+  static DEFAULT_OPTIONS = {
+    classes: [],
+    tag: "form",
+    position: {
+      width: 900,
+      height: 760
+    },
+    actions: {
+
+    }
   }
 }
